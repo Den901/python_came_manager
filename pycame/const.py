f1493d3f4cb78aa06cc5b4fc42a8c025460c0d9b
Danny Mauro <63847311+Den901@users.noreply.github.com>
2023-10-16 18:24:14 +0200

    added relays to library

---
+++ pycame/const.py
@@ -0,0 +1,17 @@
+"""Python client for ETI/Domo."""
+
+
+DEBUG_DEEP = False
+
+# Base library constants
+VERSION = "0.6.1-alpha"
+ISSUE_URL = "https://github.com/lrzdeveloper/python-came-manager/issues"
+
+STARTUP_MESSAGE = f"""
+-------------------------------------------------------------------
+CAME ETI/Domo API Python client
+Version: {VERSION}
+If you have ANY issues with this you need to open an issue here:
+{ISSUE_URL}
+-------------------------------------------------------------------
+"""
