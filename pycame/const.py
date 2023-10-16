1f7d0b7743bc494c75e35eb54a63fba04609d820
Danny Mauro <63847311+Den901@users.noreply.github.com>
2023-10-16 20:49:47 +0200

    Add files via upload

---
+++ pycame/const.py
@@ -0,0 +1,17 @@
+"""Python client for ETI/Domo."""
+
+
+DEBUG_DEEP = False
+
+# Base library constants
+VERSION = "1.0 - Beta"
+ISSUE_URL = "https://github.com/Den901/python_came_manager/issues"
+
+STARTUP_MESSAGE = f"""
+-------------------------------------------------------------------
+CAME ETI/Domo API Python client
+Version: {VERSION}
+If you have ANY issues with this you need to open an issue here:
+{ISSUE_URL}
+-------------------------------------------------------------------
+"""
