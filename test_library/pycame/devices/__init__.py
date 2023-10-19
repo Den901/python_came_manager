66eea50e2d0b74353e73d906fc86f80fd53ab2f7
Danny Mauro <63847311+Den901@users.noreply.github.com>
2023-10-19 17:09:51 +0200

    add test library with openings

---
+++ test_library/pycame/devices/__init__.py
@@ -0,0 +1,87 @@
+"""ETI/Domo devices subpackage."""
+
+import logging
+from typing import List
+
+from pycame.devices.came_analog_sensor import CameAnalogSensor
+
+from .base import CameDevice
+from .came_light import CameLight
+from .came_thermo import CameThermo
+from .came_relay import CameRelay
+from .came_opening import CameOpening
+
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def get_featured_devices(manager, feature: str) -> List[CameDevice]:
+    """Get device implementations for given feature."""
+    devices = []
+
+    if feature == "lights":
+        cmd = {
+            "cmd_name": "light_list_req",
+            "topologic_scope": "plant",
+        }
+        response = manager.application_request(cmd, "light_list_resp")
+
+        for device_info in response.get("array", []):
+            devices.append(CameLight(manager, device_info))
+
+        return devices
+
+
+    if feature == "openings":
+        cmd = {
+            "cmd_name": "openings_list_req",
+            "topologic_scope": "plant",
+        }
+        response = manager.application_request(cmd, "openings_list_resp")
+
+        for device_info in response.get("array", []):
+            devices.append(CameOpening(manager, device_info))
+
+        return devices
+
+
+
+    if feature == "relays":
+        cmd = {
+            "cmd_name": "relays_list_req",
+            "topologic_scope": "plant",
+        }
+        response = manager.application_request(cmd, "relays_list_resp")
+
+        for device_info in response.get("array", []):
+            devices.append(CameRelay(manager, device_info))
+
+        return devices
+
+
+
+
+
+    if feature == "thermoregulation":
+        cmd = {
+            "cmd_name": "thermo_list_req",
+            "topologic_scope": "plant",
+        }
+        response = manager.application_request(cmd, "thermo_list_resp")
+
+        for device_info in response.get("array", []):
+            devices.append(CameThermo(manager, device_info))
+
+        for sensor in ["temperature", "humidity", "pressure"]:
+            res = response.get(sensor)
+            if res is not None:
+                devices.append(
+                    CameAnalogSensor(
+                        manager, res, "thermo", sensor, device_class=sensor
+                    )
+                )
+
+        return devices
+
+    _LOGGER.warning("Unsupported feature type: %s", feature)
+    return []
